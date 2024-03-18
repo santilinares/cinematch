2b59d8567bfc12893ff9461a450e0719b40d3d8b
Santiago Linares <santilinares02@gmail.com>
2024-03-18 12:34:18 +0100

    Initial commit

---
+++ src/main/java/org/sd/cinematch/CinematchApplication.java
@@ -0,0 +1,13 @@
+package org.sd.cinematch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinematchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinematchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/sd/cinematch/CinematchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sd.cinematch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinematchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
