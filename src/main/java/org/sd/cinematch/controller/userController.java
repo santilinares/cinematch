5fdc6fbf7678692c391edd5dadea58b923db5137
Santiago Linares <santilinares02@gmail.com>
2024-03-19 18:09:01 +0100

    signup casi completado. Classes añadidas.

---
+++ src/main/java/org/sd/cinematch/controller/userController.java
@@ -0,0 +1,5 @@
+package org.sd.cinematch.controller;
+
+public class userController {
+    
+}
---
+++ src/main/java/org/sd/cinematch/repository/userRepository.java
@@ -0,0 +1,7 @@
+package org.sd.cinematch.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface userRepository extends JpaRepository{
+    
+}
