2042743b9e4846bacd0c7173ba4cdb5312ce02d1
Santiago Linares <santilinares02@gmail.com>
2024-04-02 23:51:10 +0200

    More changes

--- src/main/java/org/sd/cinematch/controller/FilmWebController.java
+++ src/main/java/org/sd/cinematch/controller/FilmWebController.java
@@ -11,35 +11,41 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class FilmWebController {
 
     private final FilmService filmService;
-    
-    public FilmWebController(FilmService filmService){
+
+    public FilmWebController(FilmService filmService) {
         this.filmService = filmService;
     }
 
     @GetMapping("/")
-    public String homeFilms(Model model){        
+    public String homeFilms(Model model) {
         return "home";
     }
 
     @GetMapping("/films/{id}")
-	public String showFilm(Model model, @PathVariable long id) {
-		
-            Film film = filmService.findById(id);			
-            if (film != null) {
-                model.addAttribute("film", film);
-                return "film";
-            }
-            else return "error";				
-	}
+    public String showFilm(Model model, @PathVariable long id) {
+
+        Film film = filmService.findById(id);
+        if (film != null) {
+            model.addAttribute("film", film);
+            return "film";
+        } else
+            return "error";
+    }
 
     @GetMapping("/removefilm/{id}")
-	public String removeFilm(Model model, @PathVariable long id) {
-		
-            Film removedFilm = filmService.findById(id);			
-            if (removedFilm != null) {
-                model.addAttribute("error", false);			
-                return "home";
-            }
-            else return "error";				
-	}      
+    public String removeFilm(Model model, @PathVariable long id) {
+
+        Film removedFilm = filmService.findById(id);
+        if (removedFilm != null) {
+            model.addAttribute("error", false);
+            return "home";
+        } else
+            return "error";
+    }
+
+    // Redirecciona a createFilm.html
+    @GetMapping("/createfilm")
+    public String createFilm() {
+        return "createfilm";
+    }
 }
